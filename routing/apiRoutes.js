24318660691c7273aa21636c990200d34bdcb658
Gavin Quirk <gavinquirk@gmail.com>
2018-04-19 20:16:07 -0700

    route updates

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -32,13 +32,11 @@ module.exports = function(app) {
     res.json(friendData);
   })
 
-
-  
+  app.post("/api/friends", function(req, res) {
+    friendData.push(req.body);
+    console.log(friendData)
+})
 }
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
-
 //   // API POST Requests
 //   // Below code handles when a user submits a form and thus submits data to the server.
 //   // In each of the below cases, when a user submits form data (a JSON object)
@@ -47,12 +45,12 @@ module.exports = function(app) {
 //   // Then the server saves the data to the tableData array)
 //   // ---------------------------------------------------------------------------
 
-//   app.post("/api/tables", function(req, res) {
+
 //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 //     // It will do this by sending out the value "true" have a table
 //     // req.body is available since we're using the body-parser middleware
 //     if (tableData.length < 5) {
-//       tableData.push(req.body);
+
 //       res.json(true);
 //     }
 //     else {
@@ -71,5 +69,4 @@ module.exports = function(app) {
 //     waitListData = [];
 
 //     console.log(tableData);
-//   });
-// };
+// 
