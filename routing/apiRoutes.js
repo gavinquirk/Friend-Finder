151131595ff895746c70e20fe1538e6969339f43
Gavin Quirk <gavinquirk@gmail.com>
2018-04-21 13:54:54 -0700

    added api link to front page

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,32 +1,15 @@
 
 // Your apiRoutes.js file should contain two routes:
 
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
-
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+// A GET route with the url /api/friends. This will be used to display a 
+// JSON of all possible friends.
+// A POST routes /api/friends. This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic.
 
 var friendData = require("../app/data/friends.js");
-// var waitListData = require("../data/waitinglistData");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+// Routes
 module.exports = function(app) {
-
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
@@ -36,6 +19,7 @@ module.exports = function(app) {
     friendData.push(req.body);
     console.log(friendData)
 })
+
 }
 //   // API POST Requests
 //   // Below code handles when a user submits a form and thus submits data to the server.
