14dfd6b5fa3979e9bb083c977bf375b4602b364e
Gavin Quirk <gavinquirk@gmail.com>
2018-04-21 18:15:33 -0700

    more minor changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendArray = [
     {
       'name' : 'Bob Ross',
       'photo' : "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
-      'scores' : [5, 1, 4, 4, 5, 1, 2]
+      'scores' : ['5', '1', '4', '4', '5', '1', '2']
     }
   ];
   
