01919c3946571583fc813e5de89c6c116422d3ec
Gavin Quirk <gavinquirk@gmail.com>
2018-07-10 12:54:08 -0700

    added some logic for comparison

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,24 @@ var friendArray = [
       'name' : 'Bob Ross',
       'photo' : "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
       'scores' : ['5', '1', '4', '4', '5', '1', '2']
-    }
+    },
+    {
+      'name' : 'Darth Vader',
+      'photo' : "https://reggiestake.files.wordpress.com/2012/06/darth-vader-3.jpeg",
+      'scores' : ['1', '1', '1', '1', '1', '1', '1']
+    },
+    {
+      'name' : 'Charley',
+      'photo' : "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
+      'scores' : ['2', '2', '2', '2', '2', '2', '2']
+    },
+
+
+
+
+
+
+
   ];
   
 module.exports = friendArray;
