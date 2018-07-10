412752ff7b8cd9c4f68b6208851667732fb37f03
Gavin Quirk <gavinquirk@gmail.com>
2018-07-10 13:40:08 -0700

    added more default friends to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,9 +10,24 @@ var friendArray = [
       'scores' : ['1', '1', '1', '1', '1', '1', '1']
     },
     {
-      'name' : 'Charley',
-      'photo' : "https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg",
+      'name' : 'Alex Trebek',
+      'photo' : "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fusatftw.files.wordpress.com%2F2014%2F09%2Fht_alex_trebek_mustache_jeopardy_1_jc_140911_16x9_992.jpg&f=1",
       'scores' : ['2', '2', '2', '2', '2', '2', '2']
+    },
+    {
+      'name' : 'Bill Adama',
+      'photo' : "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-ltws4f_xZw4%2FUVRrQ7GfSHI%2FAAAAAAAAm1E%2Fst-zV4DMFFI%2Fs1600%2Fbattlestar-galactica-portrait-edward-james-olmos-01-1.jpg&f=1",
+      'scores' : ['3', '3', '3', '3', '3', '3', '3']
+    },
+    {
+      'name' : 'Aeryn Sun',
+      'photo' : "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages5.fanpop.com%2Fimage%2Fphotos%2F25400000%2FAeryn-Sun-the-women-of-scifi-25436001-800-600.jpg&f=1",
+      'scores' : ['4', '4', '4', '4', '4', '4', '4']
+    },
+    {
+      'name' : 'Gimli',
+      'photo' : "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.3hobbits.com%2Flotr%2Fwallpapers%2Fgimli%2FGimli5.jpg&f=1",
+      'scores' : ['5', '5', '5', '5', '5', '5', '5']
     },
 
 
